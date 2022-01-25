9b8accf640d48c06075ffbaead27662f338ad6da
TreySchneider95 <tschneid95@gmail.com>
2022-01-24 20:51:54 -0700

    initial commit

---
+++ session_1/static/main.js
@@ -0,0 +1,35 @@
+// console.log("script");
+// let client = prompt("Name:");
+// let age = 55;
+// const URL = 'www.hello.com';
+// // URL = 'www.hey.com'; <--- Error
+// console.log(`hello ${client}`);
+// console.log(age);
+// console.log(URL);
+// document.getElementById('client').innerHTML = client
+
+let person = 'Bob';
+let num_children = 0;
+let partner_name = 'Sally';
+let geo_location = 'La Paz';
+let friend_name = 'Jim';
+let home_town = 'townsville';
+let height = "6'";
+let shoe_size = '11.5';
+let fav_sport = 'football';
+let phone_number = '555-555-5555';
+let address = '123 lane';
+let state = 'state';
+let country = 'Mexico';
+let fav_food = 'pizza';
+let species = 'human';
+let hobbie = 'Junior Developer';
+let fav_weather = 'warm';
+let fav_language = 'python';
+let fav_framework = 'Django';
+let dream_job = 'retired';
+
+
+// console.log('You will be a ' + job_title + ' in ' + geo_location + ' and married to ' + partner_name + ' with ' + num_children + ' kids');
+// console.log(`You will be a ${job_title} in ${geo_location} and married to ${partner_name} with ${num_children} kids`);
+document.write(`Info about ${person}... num_children: ${num_children} partner_name: ${partner_name} geo_location: ${geo_location} friend_name: ${friend_name} home_town: ${home_town} height: ${height} shoe_size: ${shoe_size} fav_sport: ${fav_sport} phone_number: ${phone_number} address: ${address} state: ${state} country: ${country} fav_food: ${fav_food} species: ${species} hobbie: ${hobbie} fav_weather: ${fav_weather} fav_language: ${fav_language} fav_framework: ${fav_framework} dream_job: ${dream_job}`)
